57d53c92f29313822ede4757af4cb1b650bd37ea
Deepak Kumar <deepak@korecent.com>
2022-07-19 16:07:28 +0000

    Change Ledger with View log book and udated Ledger

--- equipment_management/equipment_management/doctype/equipment/equipment.js
+++ equipment_management/equipment_management/doctype/equipment/equipment.js
@@ -33,40 +33,44 @@ frappe.ui.form.on('Equipment', {
 			$('.indicator-pill.whitespace-nowrap').removeClass('gray')
 			$('.indicator-pill.whitespace-nowrap').addClass('red')
 		}
-		cur_frm.add_custom_button(__('Ledger'), function () {
+		cur_frm.add_custom_button(__('View Log Book'), function () {
 			frappe.set_route('query-report', 'Equipment Ledger',
 					{equipment: frm.doc.name});
 		});
 
 		cur_frm.add_custom_button(__('Report a Problem'), function () {
-			frappe.set_route('problem-report','new-problem-report-1',
+			frappe.set_route('problem-report','new-problem-report',
 					{equipment: frm.doc.name});
 			
 		},__('Problem & Maintenance'));
 
 		cur_frm.add_custom_button(__('Perform Equipment Maintenance'), function () {
-			frappe.set_route('equipment-maintenance','new-equipment-maintenance-1',
+			frappe.set_route('equipment-maintenance','new-equipment-maintenance',
 					{equipment: frm.doc.name});
 			
 		},__('Problem & Maintenance'));
 
 		cur_frm.add_custom_button(__('Scrap'), function () {
-			frappe.set_route('manual-equipment-movement','new-manual-equipment-movement-1',
+			frappe.set_route('manual-equipment-movement','new-manual-equipment-movement',
 					{equipment: frm.doc.name, type: 'Scrap'});
 			
 		},__('Manual Equipment Movement'));
 
 		cur_frm.add_custom_button(__('Loan'), function () {
-			frappe.set_route('manual-equipment-movement','new-manual-equipment-movement-1',
+			frappe.set_route('manual-equipment-movement','new-manual-equipment-movement',
 					{equipment: frm.doc.name, type: 'Loan'});
 			
 		},__('Manual Equipment Movement'));
 
 		cur_frm.add_custom_button(__('Send to Repair'), function () {
-			frappe.set_route('manual-equipment-movement','new-manual-equipment-movement-1',
+			frappe.set_route('manual-equipment-movement','new-manual-equipment-movement',
 					{equipment: frm.doc.name, type: 'Repair'});
 			
 		},__('Manual Equipment Movement'));
+	
+		if (!frappe.user.has_role('System Manager') && frappe.user.has_role('eusectra basic User')) {
+			frm.set_df_property('status', 'hidden',1)
+		}
 		
 	}
 });
---
+++ equipment_management/equipment_management/report/equipment_report/equipment_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Deepak Kumar and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Equipment Report"] = {
+	"filters": [
+
+	]
+};
