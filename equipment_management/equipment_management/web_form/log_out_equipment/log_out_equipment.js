994a1ed9b3f1432d82afd34d980b31dc57489044
Deepak Kumar <deepak@korecent.com>
2022-07-29 11:17:12 +0000

    web form modified directory

--- equipment_management/equipment_management/web_form/log_out_equipment/log_out_equipment.js
+++ equipment_management/equipment_management/web_form/log_out_equipment/log_out_equipment.js
@@ -1,3 +1,6 @@
 frappe.ready(function() {
 	// bind events here
+	frappe.web_form.after_load = () => {
+		frappe.web_form.set_value('posting_time',frappe.datetime.now_time())
+	}
 })
--- equipment_management/public/js/web_form/equipment_maintenance.js
+++
@@ -1,4 +0,0 @@
-setTimeout(() => {
-    frappe.web_form.set_value('posting_time',frappe.datetime.now_time())
-}, 100);
-
