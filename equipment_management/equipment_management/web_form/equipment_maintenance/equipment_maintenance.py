7f4f3045ed0c389fd9a00e318fb1a98d8097ffa6
Deepak Kumar <deepak@korecent.com>
2022-08-03 05:15:14 +0000

    update list in equipment maintenance web form

--- equipment_management/equipment_management/web_form/equipment_maintenance/equipment_maintenance.py
+++ equipment_management/equipment_management/web_form/equipment_maintenance/equipment_maintenance.py
@@ -5,3 +5,9 @@ import frappe
 def get_context(context):
 	# do your magic here
 	pass
+def get_list_context(context):
+	context.get_list = get_equipment_maintenance_list
+
+def get_equipment_maintenance_list(doctype, txt, filters, limit_start, limit_page_length, order_by):
+	data = frappe.db.sql(""" select name, status, due_date from `tabEquipment Maintenance` where status='Pending' """,as_dict=1)
+	return data
